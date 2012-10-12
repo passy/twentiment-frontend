1fcb33147c24b0b057b68a52e7a907c017771f46
Pascal Hartig <phartig@rdrei.net>
2012-10-13 01:14:07 +0200

    Added start and pause buttons

--- app/scripts/controller/search.js
+++ app/scripts/controller/search.js
@@ -3,21 +3,54 @@
         app.controller('SearchController', ['$scope', 'twitterSearchStream',
                        function ($scope, twitterSearchStream) {
 
-
-            var stream = twitterSearchStream(":)"),
+            var stream,
+                lastQuery,
+                MAX_TWEETS = 50,
+                // Keep track of seen tweets
+                closed = {},
                 mergeTweets = function (response) {
                     _.map(response.results, function (tweet) {
-                        $scope.tweets.unshift(tweet);
+                        if (!closed[tweet.id_str]) {
+                            $scope.tweets.unshift(tweet);
+                            closed[tweet.id_str] = 1;
+                        }
                     });
+                    $scope.tweets = $scope.tweets.slice(0, MAX_TWEETS);
+                },
+                clear = function () {
+                    $scope.tweets = [];
                 };
 
             $scope.tweets = [];
+            $scope.running = false;
 
-            stream.start(5000, function (promise) {
-                promise.then(function (response) {
-                    mergeTweets(response);
-                });
+            $scope.$watch('query', function () {
+                $scope.pause();
             });
+
+
+            $scope.start = function () {
+                $scope.running = true;
+
+                if (lastQuery !== $scope.query) {
+                    clear();
+                }
+
+                lastQuery = $scope.query;
+                stream = twitterSearchStream($scope.query);
+                stream.start(5000, function (promise) {
+                    promise.then(function (response) {
+                        mergeTweets(response);
+                    });
+                });
+            };
+
+            $scope.pause = function () {
+                $scope.running = false;
+                if (stream) {
+                    stream.stop();
+                }
+            };
         }]);
     });
 }());
